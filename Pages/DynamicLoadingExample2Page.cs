f9be6d1b46c5cb6f37129ae719abf6ba19f6f683
MUHAMMAD BILAL <m.b1lal@hotmail.com>
2023-11-15 00:45:21 +0400

    Added dynamic loading tests

---
+++ Pages/DynamicLoadingExample2Page.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using SeleniumExtras.WaitHelpers;
+
+namespace DotNetTestingFramework.Pages
+{
+    public class DynamicLoadingExample2Page
+    {
+        private IWebDriver _driver;
+        [FindsBy(How = How.CssSelector, Using = "#start > button")]
+        private IWebElement? btnStart;
+
+        private By loadingBar = By.Id("loading");
+
+        [FindsBy(How = How.Id, Using = "finish")]
+        private IWebElement? txtHello;
+
+        public DynamicLoadingExample2Page(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        public void StartLoading() => btnStart.Click();
+
+        private void WaitForLoadingToFinish()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, System.TimeSpan.FromSeconds(30));
+            wait.Until(ExpectedConditions.InvisibilityOfElementLocated(loadingBar));
+        }
+
+        public string GetInvisibleELementText()
+        {
+            WaitForLoadingToFinish();
+            return txtHello.Text;
+        }
+    }
+}
